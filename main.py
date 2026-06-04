d1202e0cce61f02486dd1214bec4cf604f71d5be
Jyoti <jyotimandal9354@gmail.com>
2026-06-04 11:39:02 +0530

    Completed Student Performance Prediction System

--- main.py
+++ main.py
@@ -3,30 +3,24 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 
-# Load Dataset
 data = pd.read_csv("student_data.csv")
 
 print("Dataset:")
 print(data)
 
-# Features and Target
 X = data[['StudyHours', 'Attendance', 'PreviousScore']]
-y = data['Pass']
+y = data['Result']
 
-# Split Dataset
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# Train Model
 model = DecisionTreeClassifier()
 model.fit(X_train, y_train)
 
-# Prediction
 predictions = model.predict(X_test)
 
-# Accuracy
 accuracy = accuracy_score(y_test, predictions)
 
-print("\nModel Accuracy:", accuracy)
- 
+print("\nPredictions:", predictions)
+print("Model Accuracy:", accuracy)
